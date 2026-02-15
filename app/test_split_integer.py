53f3e35fccb63c8bfdb760c0bf24ff50c9bc1873
Ivan <ivanqqq@ukr.net>
2026-02-15 11:22:12 +0100

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,99 @@
 from app.split_integer import split_integer
+import pytest
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+class TestSplitInteger:
+
+    @pytest.mark.parametrize(
+        "value,parts",
+        [
+            (8, 1),
+            (6, 2),
+            (17, 4),
+            (32, 6),
+        ],
+        ids=[
+            "sum of parts should be 8",
+            "sum of parts should be 6",
+            "sum of parts should be 17",
+            "sum of parts should be 32"
+        ]
+    )
+    def test_sum_of_the_parts_should_be_equal_to_value(self, value, parts) -> None:
+        sum_of_the_parts = sum(split_integer(value, parts))
+        assert sum_of_the_parts == value, f"sum of parts does not equal {value}"
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    @pytest.mark.parametrize(
+        "value,parts",
+        [
+            (9, 3),
+            (16, 4),
+            (25, 5),
+            (121, 11),
+        ],
+        ids=[
+            "all parts and their amount should equal 3",
+            "all parts and their amount should equal 4",
+            "all parts and their amount should equal 5",
+            "all parts and their amount should equal 11"
+        ]
+    )
+    def test_should_split_into_equal_parts_when_value_divisible_by_parts(self, value, parts) -> None:
+        parts_list = split_integer(value, parts)
+        assert len(parts_list) == parts, f"number of parts should equal {parts}"
+        for part in range(0, len(parts_list) - 1):
+            assert parts_list[part] == parts_list[part + 1], f"{parts_list[part]} does not equal {part + 1}"
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    @pytest.mark.parametrize(
+    "value,parts",
+        [
+            (1, 1),
+            (5, 1),
+            (10, 1),
+            (100, 1),
+        ],
+        ids=[
+            "result should be [1]",
+            "result should be [5]",
+            "result should be [10]",
+            "result should be [100]"
+        ]
+    )
+    def test_should_return_part_equals_to_value_when_split_into_one_part(self, value, parts) -> None:
+        parts_list = split_integer(value, parts)
+        assert len(parts_list) == 1, f"result should contain 1 part"
+        assert parts_list == [value], f"result does not equal {value}"
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    @pytest.mark.parametrize(
+        "value,parts,parts_list",
+        [
+            (17, 4, [4, 4, 4, 5]),
+            (32, 6, [5, 5, 5, 5, 6, 6]),
+        ],
+        ids=[
+            "result should be [4, 4, 4, 5]",
+            "result should be [5, 5, 5, 5, 6, 6]"
+        ]
+    )
+    def test_parts_should_be_sorted_when_they_are_not_equal(self, value, parts, parts_list) -> None:
+        assert parts_list == split_integer(value, parts), f"{parts} should be sorted"
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    @pytest.mark.parametrize(
+       "value,parts,parts_list",
+       [
+           (1, 2, [0, 1]),
+           (3, 4, [0, 1, 1, 1]),
+           (10, 11, [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
+       ],
+       ids=[
+           "result should be [0, 1]",
+           "result should be [0, 1, 1, 1]",
+           "result should be [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]"
+       ]
+    )
+    def test_should_add_zeros_when_value_is_less_than_number_of_parts(self, value, parts, parts_list) -> None:
+        assert split_integer(value, parts) == parts_list, f"result should be [0, 1, 1, ... 1]"
